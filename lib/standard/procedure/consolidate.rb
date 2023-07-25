4764a72e407a8fa8ef082cf030444b911bbbdf9a
Rahoul Baruah <rahoulb@echodek.co>
2023-07-25 16:50:03 +0100

    festlegen

---
+++ lib/standard/procedure/consolidate.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "consolidate/version"
+
+module Standard
+  module Procedure
+    module Consolidate
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/standard/procedure/consolidate/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Standard
+  module Procedure
+    module Consolidate
+      VERSION = "0.1.0"
+    end
+  end
+end
