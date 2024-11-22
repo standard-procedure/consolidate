9957e6f226730dc57eb466771f7704a877de5791
Rahoul Baruah <rahoulb@echodek.co>
2024-11-22 13:52:16 +0000

    Tidied up the code
    Moved the block in .open as it did not belong in #initialize.
    
    Converted some methods to endless, because I like them
    
    Centralised the regex for matching tags a bit.
    
    Removed some debug messages

--- lib/consolidate/docx/merge.rb
+++ lib/consolidate/docx/merge.rb
@@ -7,39 +7,32 @@ module Consolidate
   module Docx
     class Merge
       def self.open(path, verbose: false, &block)
-        new(path, verbose: verbose, &block)
+        new(path, verbose: verbose, &block).tap do |merge|
+          block&.call merge
+        end
         path
       end
 
-      def initialize(path, verbose: false, &block)
+      def initialize(path, verbose: false)
         @verbose = verbose
         @output = {}
         @zip = Zip::File.open(path)
         @documents = load_documents
-        block&.call self
       end
 
       # Helper method to display the contents of the document and the merge fields from the CLI
       def examine
-        documents = document_names.join(", ")
-        fields = field_names.join(", ")
-        puts "Documents: #{documents}"
-        puts "Merge fields: #{fields}"
+        puts "Documents: #{document_names.join(", ")}"
+        puts "Merge fields: #{field_names.join(", ")}"
       end
 
       # Read all documents within the docx and extract any merge fields
-      def field_names
-        tag_nodes.collect do |tag_node|
-          field_names_from tag_node
-        end.flatten.compact.uniq
-      end
+      def field_names = tag_nodes.collect { |tag_node| field_names_from tag_node }.flatten.compact.uniq
 
       # List the documents stored within this docx
-      def document_names
-        @zip.entries.collect { |entry| entry.name }
-      end
+      def document_names = @zip.entries.collect { |entry| entry.name }
 
-      # Substitute the data from the merge fields with the values provided
+      # Perform the substitution - creating copies of any documents that contain merge tags and replacing the tags with the supplied data
       def data mapping = {}
         mapping = mapping.transform_keys(&:to_s)
 
@@ -52,18 +45,16 @@ module Consolidate
 
         @documents.each do |name, document|
           output_document = substitute document.dup, mapping: mapping, document_name: name
-
           @output[name] = output_document.serialize save_with: 0
         end
       end
 
-      # Write the new document to the given path
       def write_to path
         puts "...writing to #{path}" if verbose
         Zip::File.open(path, Zip::File::CREATE) do |out|
-          zip.each do |entry|
+          @zip.each do |entry|
             out.get_output_stream(entry.name) do |o|
-              o.write(output[entry.name] || zip.read(entry.name))
+              o.write(@output[entry.name] || @zip.read(entry.name))
             end
           end
         end
@@ -72,54 +63,38 @@ module Consolidate
       private
 
       attr_reader :verbose
-      attr_reader :zip
-      attr_reader :xml
-      attr_reader :documents
-      attr_accessor :output
-      TAG = /\{\{\s*(\S+)\s*\}\}/
+
+      def any_tag = /\{\{\s*(\S+)\s*\}\}/
+
+      def tag_for(field_name) = /\{\{\s*#{field_name}\s*\}\}/
 
       def load_documents
-        @zip.entries.each_with_object({}) do |entry, documents|
+        @documents = @zip.entries.each_with_object({}) do |entry, results|
           next unless entry.name.match?(/word\/(document|header|footer|footnotes|endnotes).?\.xml/)
           puts "...reading #{entry.name}" if verbose
           xml = @zip.get_input_stream entry
-          documents[entry.name] = Nokogiri::XML(xml) { |x| x.noent }
+          results[entry.name] = Nokogiri::XML(xml) { |x| x.noent }
         end
       ensure
         @zip.close
       end
 
-      # Collect all the nodes that contain merge fields
-      def tag_nodes
-        documents.collect do |name, document|
-          tag_nodes_for document
-        end.flatten
-      end
+      def tag_nodes = @documents.collect { |name, document| tag_nodes_for document }.flatten
 
-      # go through all w:t (Word Text???) nodes of the document
-      # find any nodes that contain "{{"
-      # then find the ancestor node that also includes the ending "}}"
-      # This collection of nodes contains all the merge fields for this document
-      def tag_nodes_for document
-        (document / "//w:p").select do |paragraph|
-          paragraph.content.match(TAG)
-        end
-      end
+      # go through all paragraph nodes of the document
+      # selecting any that contain a merge tag
+      def tag_nodes_for(document) = (document / "//w:p").select { |paragraph| paragraph.content.match(any_tag) }
 
-      # Extract the merge field name from the node
-      def field_names_from(tag_node)
-        matches = tag_node.content.scan(TAG)
-        matches.empty? ? nil : matches.flatten.map(&:strip)
-      end
+      # Extract the merge field name(s) from the paragraph
+      def field_names_from(tag_node) = (matches = tag_node.content.scan(any_tag)).empty? ? nil : matches.flatten.map(&:strip)
 
       # Go through the given document, replacing any merge fields with the values provided
       # and storing the results in a new document
       def substitute document, document_name:, mapping: {}
         tag_nodes_for(document).each do |tag_node|
           field_names = field_names_from tag_node
-          puts "Original Node for #{field_names} is #{tag_node}" if verbose
 
-          # Extract the paragraph properties node if it exists
+          # Extract the properties (formatting) nodes if they exist
           paragraph_properties = tag_node.search ".//w:pPr"
           run_properties = tag_node.at_xpath ".//w:rPr"
 
@@ -127,20 +102,20 @@ module Consolidate
           field_names.each do |field_name|
             field_value = mapping[field_name].to_s
             puts "...substituting #{field_name} with #{field_value} in #{document_name}" if verbose
-            text = text.gsub(/{{\s*#{field_name}\s*}}/, field_value)
+            text = text.gsub(tag_for(field_name), field_value)
           end
 
           # Create a new text node with the substituted text
           text_node = Nokogiri::XML::Node.new("w:t", tag_node.document)
           text_node.content = text
 
-          # Create a new run node to hold the substituted text and the paragraph properties
+          # Create a new run node to hold the run properties and substitute text node
           run_node = Nokogiri::XML::Node.new("w:r", tag_node.document)
           run_node << run_properties if run_properties
           run_node << text_node
-          tag_node.children = Nokogiri::XML::NodeSet.new(document, paragraph_properties.to_a + [run_node])
 
-          puts "TAG NODE FOR #{field_names} IS #{tag_node}" if verbose
+          # Add the paragraph properties and the run node to the tag node
+          tag_node.children = Nokogiri::XML::NodeSet.new(document, paragraph_properties.to_a + [run_node])
         rescue => ex
           # Have to mangle the exception message otherwise it outputs the entire document
           puts ex.message.to_s[0..255]
