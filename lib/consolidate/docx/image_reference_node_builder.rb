9b7f1ab1a58e78a42cb85bacc675155971caf26e
Rahoul Baruah <rahoulb@echodek.co>
2025-02-24 14:57:45 +0000

    Use Anchor for embedding images instead of Inline

--- lib/consolidate/docx/image_reference_node_builder.rb
+++ lib/consolidate/docx/image_reference_node_builder.rb
@@ -12,27 +12,58 @@ module Consolidate
 
         Nokogiri::XML::Node.new("w:drawing", document).tap do |drawing|
           drawing["xmlns:a"] = "http://schemas.openxmlformats.org/drawingml/2006/main"
-          drawing << Nokogiri::XML::Node.new("wp:inline", document).tap do |inline|
-            inline["distT"] = "0"
-            inline["distB"] = "0"
-            inline["distL"] = "0"
-            inline["distR"] = "0"
-            inline << Nokogiri::XML::Node.new("wp:extent", document).tap do |extent|
+          drawing << Nokogiri::XML::Node.new("wp:anchor", document).tap do |anchor|
+            anchor["behindDoc"] = "0"
+            anchor["distT"] = "0"
+            anchor["distB"] = "0"
+            anchor["distL"] = "0"
+            anchor["distR"] = "0"
+            anchor["simplePos"] = "0"
+            anchor["locked"] = "0"
+            anchor["layoutInCell"] = "0"
+            anchor["allowOverlap"] = "1"
+            anchor["relativeHeight"] = "2"
+            anchor << Nokogiri::XML::Node.new("wp:simplePos", document).tap do |pos|
+              pos["x"] = "0"
+              pos["y"] = "0"
+            end
+            anchor << Nokogiri::XML::Node.new("wp:positionH", document).tap do |posh|
+              posh["relativeFrom"] = "column"
+              posh << Nokogiri::XML::Node.new("wp:align", document).tap do |align|
+                align.content = "center"
+              end
+            end
+            anchor << Nokogiri::XML::Node.new("wp:positionV", document).tap do |posv|
+              posv["relativeFrom"] = "paragraph"
+              posv << Nokogiri::XML::Node.new("wp:posOffset", document).tap do |offset|
+                offset.content = "635"
+              end
+            end
+            anchor << Nokogiri::XML::Node.new("wp:extent", document).tap do |extent|
               extent["cx"] = scaled_width
               extent["cy"] = scaled_height
             end
-            inline << Nokogiri::XML::Node.new("wp:effectExtent", document).tap do |effect_extent|
+            anchor << Nokogiri::XML::Node.new("wp:effectExtent", document).tap do |effect_extent|
               effect_extent["l"] = "0"
               effect_extent["t"] = "0"
               effect_extent["r"] = "0"
               effect_extent["b"] = "0"
             end
-            inline << Nokogiri::XML::Node.new("wp:cNvGraphicFramePr", document).tap do |c_nv_graphic_frame_pr|
+            anchor << Nokogiri::XML::Node.new("wp:wrapSquare", document).tap do |wrap_square|
+              wrap_square["wrapText"] = "largest"
+            end
+            anchor << Nokogiri::XML::Node.new("wp:docPr", document).tap do |doc_pr|
+              doc_pr["id"] = image_number
+              doc_pr["name"] = image.name
+              doc_pr["descr"] = ""
+              doc_pr["title"] = ""
+            end
+            anchor << Nokogiri::XML::Node.new("wp:cNvGraphicFramePr", document).tap do |c_nv_graphic_frame_pr|
               c_nv_graphic_frame_pr << Nokogiri::XML::Node.new("a:graphicFrameLocks", document).tap do |graphic_frame_locks|
                 graphic_frame_locks["noChangeAspect"] = true
               end
             end
-            inline << Nokogiri::XML::Node.new("a:graphic", document).tap do |graphic|
+            anchor << Nokogiri::XML::Node.new("a:graphic", document).tap do |graphic|
               graphic["xmlns:a"] = "http://schemas.openxmlformats.org/drawingml/2006/main"
               graphic << Nokogiri::XML::Node.new("a:graphicData", document).tap do |graphic_data|
                 graphic_data["uri"] = "http://schemas.openxmlformats.org/drawingml/2006/picture"
@@ -44,7 +75,12 @@ module Consolidate
                       c_nv_pr["name"] = image.name
                       c_nv_pr["descr"] = image.name
                       c_nv_pr["hidden"] = false
-                      c_nv_pr << Nokogiri::XML::Node.new("pic:cNvPicPr", document)
+                      c_nv_pr << Nokogiri::XML::Node.new("pic:cNvPicPr", document).tap do |c_nv_pic_pr|
+                        c_nv_pic_pr << Nokogiri::XML::Node.new("a:picLocks", document).tap do |pic_locks|
+                          pic_locks["noChangeAspect"] = "1"
+                          pic_locks["noChangeArrowheads"] = "1"
+                        end
+                      end
                     end
                   end
                   pic << Nokogiri::XML::Node.new("pic:blipFill", document).tap do |blip_fill|
@@ -56,6 +92,7 @@ module Consolidate
                     end
                   end
                   pic << Nokogiri::XML::Node.new("pic:spPr", document).tap do |sp_pr|
+                    sp_pr["bwMode"] = "auto"
                     sp_pr << Nokogiri::XML::Node.new("a:xfrm", document).tap do |xfrm|
                       xfrm << Nokogiri::XML::Node.new("a:off", document).tap do |off|
                         off["x"] = "0"
@@ -70,6 +107,7 @@ module Consolidate
                       prst_geom["prst"] = "rect"
                       prst_geom << Nokogiri::XML::Node.new("a:avLst", document)
                     end
+                    sp_pr << Nokogiri::XML::Node.new("a:noFill", document)
                   end
                 end
               end
